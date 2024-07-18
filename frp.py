c78e835d590dbedef0af17ae8bf843523d9d1ce1
nyan9 <2575044704@qq.com>
2024-07-18 21:24:26 +0800

    Update frp.py

--- frp.py
+++ frp.py
@@ -18,6 +18,7 @@ frp_token = "****"   # 这里填服务器密码（token）
 port1 = "00000"   # 这里填第一个端口
 port2 = "00000"   # 这里填第二个端口
 
+
 config = f"""
 [common]
 server_addr = 154.44.10.30
@@ -30,12 +31,13 @@ local_ip = 127.0.0.1
 local_port = 7860
 remote_port = {port1} 
 
-[sdwebuip{port2}] 
+[sdwebuip_{port2}] 
 type = tcp
 local_ip = 127.0.0.1
 local_port = 7861
 remote_port = {port2} 
 """
+print(config)
 
 file_path = './cyanfrp.ini'
 
@@ -44,12 +46,13 @@ with open(file_path, 'w') as config_file:
     config_file.write(config)
 
 print(f"配置文件已创建为 {file_path}")
-def install_Frpc(port, frpconfigfile, use_frpc):
+def install_Frpc(port, use_frpc):
     if use_frpc:
         !cp /kaggle/input/d/yiyiooo/net-tools/frpc /kaggle/working
         !cp /kaggle/input/net-tools/frpc /kaggle/working
         subprocess.run(['chmod', '+x', '/kaggle/working/frpc'], check=True)
         print(f'正在启动frp ，端口{port}')
+        !sleep 2 
         subprocess.Popen(['/kaggle/working/frpc', '-c', './cyanfrp.ini'])
-        
-install_Frpc('5140',frpconfigfile,use_frpc)
+port_all = port1 + "、" + port2
+install_Frpc(port_all, use_frpc)
