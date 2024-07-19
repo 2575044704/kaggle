cd51e7a4dfb17e8f414ec29de86d4bcf2e9d03f2
nyan9 <2575044704@qq.com>
2024-07-20 01:20:59 +0800

    Update frp.py

--- frp.py
+++ frp.py
@@ -1,13 +1,14 @@
 # 使用的库
 import subprocess
 import os
+import threading
 
 use_frpc = True
 frp_token = "****"   # 这里填服务器密码（token）
 port1 = "00000"   # 这里填第一个端口
 port2 = "00000"   # 这里填第二个端口
 
-config = f"""
+config1 = f"""
 [common]
 server_addr = 45.194.32.78
 server_port = 7000
@@ -18,6 +19,13 @@ type = tcp
 local_ip = 127.0.0.1
 local_port = 7860
 remote_port = {port1} 
+"""
+
+config2 = f"""
+[common]
+server_addr = 45.194.32.78
+server_port = 7000
+token = {frp_token} 
 
 [sdwebuip_{port2}] 
 type = tcp
@@ -25,24 +33,26 @@ local_ip = 127.0.0.1
 local_port = 7861
 remote_port = {port2} 
 """
-print(config)
 
-file_path = './cyanfrp.ini'
-
-# 将配置内容写入文件
-with open(file_path, 'w') as config_file:
-    config_file.write(config)
-
-print(f"配置文件已创建为 {file_path}")
-
-def install_Frpc(port, use_frpc):
+with open('./cyanfrp1.ini', 'w') as config_file:
+    config_file.write(config1)
+with open('./cyanfrp2.ini', 'w') as config_file:
+    config_file.write(config2)
+print(f"配置文件已创建")
+subprocess.run(['cp', '/kaggle/input/d/yiyiooo/net-tools/frpc', '/kaggle/working'])
+subprocess.run(['cp', '/kaggle/input/net-tools/frpc', '/kaggle/working'])
+subprocess.run(['chmod', '+x', '/kaggle/working/frpc'], check=True)
+def install_Frpc(file_path, port, use_frpc, log_file_path):
     if use_frpc:
-        subprocess.run(['cp', '/kaggle/input/d/yiyiooo/net-tools/frpc', '/kaggle/working'])
-        subprocess.run(['cp', '/kaggle/input/net-tools/frpc', '/kaggle/working'])
-        subprocess.run(['chmod', '+x', '/kaggle/working/frpc'], check=True)
         print(f'正在启动frp ，端口{port}')
-        subprocess.run(['sleep', '2'])
-        subprocess.Popen(['/kaggle/working/frpc', '-c', './cyanfrp.ini'])
+        with open(log_file_path, 'w') as log_file:
+            process = subprocess.Popen(['/kaggle/working/frpc', '-c', file_path], stdout=log_file, stderr=log_file)
+        subprocess.run(['sleep', '4'])
+        subprocess.run(['cat', log_file_path])
+thread1 = threading.Thread(target=install_Frpc, args=('./cyanfrp1.ini', port1, use_frpc, '/kaggle/frp_log1.txt'))
+thread2 = threading.Thread(target=install_Frpc, args=('./cyanfrp2.ini', port2, use_frpc, '/kaggle/frp_log2.txt'))
+thread1.start()
+thread2.start()
+thread1.join()
+thread2.join()
 
-port_all = port1 + "、" + port2
-install_Frpc(port_all, use_frpc)
